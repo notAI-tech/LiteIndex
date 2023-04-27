6f0a1054995440e2c950e9796f82d6e3941349ca
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2023-04-27 19:58:13 +0530

    add ci
    Signed-off-by: Bedapudi Praneeth <praneethbedapudi@gmail.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/notAI-tech/liteindex"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.1"
+VERSION = "0.0.1.dev1"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
