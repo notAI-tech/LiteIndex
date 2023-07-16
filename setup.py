d85b1fe80abb06bae7bc218d238b3ce1e9736032
Praneeth Bedapudi <praneeth@bpraneeth.com>
2023-07-16 19:42:55 +0530

    fix select columns
    Signed-off-by: Praneeth Bedapudi <praneeth@bpraneeth.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/notAI-tech/liteindex"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.1.dev22"
+VERSION = "0.0.1.dev23"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
