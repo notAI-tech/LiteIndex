870c0e7c01c778a7d4d31c2d3d11e788906a9941
Praneeth Bedapudi <praneeth@bpraneeth.com>
2023-12-11 14:15:13 +0530

    add lmdb to reqs
    Signed-off-by: Praneeth Bedapudi <praneeth@bpraneeth.com>

--- setup.py
+++ setup.py
@@ -18,11 +18,12 @@ URL = "https://github.com/notAI-tech/liteindex"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.2.dev24"
+VERSION = "0.0.2.dev25"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "zstandard"
+    "zstandard",
+    "lmdb"
 ]
 
 # What packages are optional?
